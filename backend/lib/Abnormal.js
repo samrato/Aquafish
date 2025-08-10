4d6f8957c3db53a3cffc98f499984a5eaccaa440
Willington Juma <146538882+samrato@users.noreply.github.com>
2025-08-10 18:00:33 +0300

    Update Abnormal.js

--- backend/lib/Abnormal.js
+++ backend/lib/Abnormal.js
@@ -1,13 +1,13 @@
 export default function checkAbnormality(data) {
     let abnormal = '';
 
-    if (data.oxygen < 5) {
+    if (data.oxygen < 0) {
         abnormal += `⚠️ Oxygen level dropped below normal (5 mg/L) to ${data.oxygen} mg/L.<br/>`;
     }
-    if (data.nitrogen > 0.1) {
+    if (data.nitrogen > 0) {
         abnormal += `⚠️ Nitrogen level exceeded safe limit (0.1 mg/L) to ${data.nitrogen} mg/L.<br/>`;
     }
-    if (data.phosphorus > 0.1) {
+    if (data.phosphorus > 0) {
         abnormal += `⚠️ Phosphorus level exceeded safe limit (0.1 mg/L) to ${data.phosphorus} mg/L.<br/>`;
     }
     if (data.temp > 37 || data.temp < 26) {
